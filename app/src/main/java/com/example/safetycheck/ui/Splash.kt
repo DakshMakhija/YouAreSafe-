8a1e1e2423962855835b93224e37e7e9ddff5dfe
IamBansal <abansal11ae@gmail.com>
2022-09-23 13:30:06 +0530

    UI Fix

--- app/src/main/java/com/example/safetycheck/ui/Splash.kt
+++ app/src/main/java/com/example/safetycheck/ui/Splash.kt
@@ -4,15 +4,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.safetycheck.R
 import com.example.safetycheck.auth.Login
 
 class Splash : AppCompatActivity() {
+
+    private lateinit var logo : ImageView
+    private lateinit var head : TextView
+    private lateinit var smile : ImageView
+    private lateinit var text : TextView
+    private lateinit var animation: Animation
+    private lateinit var fadeAnimation: Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler().postDelayed({
+        logo = findViewById(R.id.ivSplash)
+        head = findViewById(R.id.tvSplash)
+        smile = findViewById(R.id.tv3Splash)
+        text = findViewById(R.id.tv2Splash)
+        animation =  AnimationUtils.loadAnimation(this, R.anim.logo_anim)
+        fadeAnimation =  AnimationUtils.loadAnimation(this, R.anim.fade)
+
+        logo.animation = animation
+//        head.animation = animation
+        smile.animation = fadeAnimation
+        text.animation = fadeAnimation
+
+        Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, Login::class.java))
         }, 2500)
 
